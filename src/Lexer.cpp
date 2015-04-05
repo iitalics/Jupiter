59f33cedd91a667b174ca7df1cebf2f40ff30b8e
Milo Turner <iitalics@gmail.com>
2015-04-04 22:18:08 -1000

    Added lexer

---
+++ src/Lexer.cpp
@@ -0,0 +1,497 @@
+#include "Lexer.h"
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+
+// configurable?
+
+enum
+{
+	DIGIT_MASK = 1,
+	CONTROL_MASK = 2,
+	SPACE_MASK = 4,
+	IDENT_MASK = 8,
+	OPERATOR_MASK = 16,
+};
+
+#ifndef NO_KEY_MAPS
+#define USE_KEY_MAP
+#endif
+
+#ifdef USE_KEY_MAP
+
+static int key_map[128] = {
+	4,24,24,24,24,24,24,24,4,4,4,24,24,4,24,24,
+	24,24,24,24,24,24,24,24,24,24,24,24,24,24,24,24,
+	4,24,0,24,24,24,24,24,2,2,24,24,2,24,2,24,
+	25,25,25,25,25,25,25,25,25,25,24,2,24,24,24,24,
+	24,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,
+	8,8,8,8,8,8,8,8,8,8,8,2,2,2,24,24,
+	24,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,
+	8,8,8,8,8,8,8,8,8,8,8,2,24,2,24,24,
+};
+
+
+
+
+static bool is_digit (char c)    { return key_map[int(c)] & DIGIT_MASK; }
+static bool is_control (char c)  { return key_map[int(c)] & CONTROL_MASK; }
+static bool is_space (char c)    { return key_map[int(c)] & SPACE_MASK; }
+static bool is_ident (char c)    { return key_map[int(c)] & IDENT_MASK; }
+static bool is_operator (char c) { return key_map[int(c)] & OPERATOR_MASK; }
+
+#else
+
+static bool is_digit (char c)
+{
+	return c >= '0' && c <= '9';
+}
+static bool is_control (char c)
+{
+	return c == tDot || c == tComma ||
+		c == tLambda || c == tSemicolon ||
+		c == tLParen || c == tRParen ||
+		c == tLCurl  || c == tRCurl ||
+		c == tLBrack || c == tRBrack;
+}
+static bool is_space (char c)
+{
+	return c == ' '  || c == '\t' ||
+	       c == '\n' || c == '\r' ||
+	       c == '\b' || c == '\0';
+}
+static bool is_ident (char c)
+{
+	return c != '\"' && !is_space(c) && !is_control(c);
+}
+static bool is_operator (char c)
+{
+	return is_ident(c) &&
+	       !((c >= 'a' && c <= 'z') ||
+		     (c >= 'A' && c <= 'Z'));
+}
+
+#endif
+
+static const char COMMENT = '#';
+
+std::vector<std::pair<std::string, int>> Lexer::keywords {
+	{ "if",    tIf },
+	{ "then",  tThen },
+	{ "else",  tElse },
+	{ "func",  tFunc },
+	{ "let",   tLet },
+	{ "loop",  tLoop },
+	{ "for",   tFor },
+	{ "true",  tTrue },
+	{ "false", tFalse },
+	{ "=",     tEqual },
+	{ "_",     tWildcard },
+	{ ":",     tColon },
+	{ "->",    tArrow },
+//	{ "=>",    tFatArrow }
+};
+
+static const char PRETTY_QUOTE = '\'';
+static const char PRETTY_QUOTE_KEYWORD = '`';
+static const size_t MAX_STRING_DISPLAY_LEN = 5;
+
+
+
+
+
+
+
+
+void Lexer::generateKeyMaps ()
+{
+	const int NKEYS = 128;
+
+	int map[NKEYS];
+
+	size_t i;
+
+	for (i = 0; i < NKEYS; i++)
+	{
+		map[i] = 0;
+
+		if (is_digit(char(i)))
+			map[i] |= DIGIT_MASK;
+		if (is_control(char(i)))
+			map[i] |= CONTROL_MASK;
+		if (is_space(char(i)))
+			map[i] |= SPACE_MASK;
+		if (is_ident(char(i)))
+			map[i] |= IDENT_MASK;
+		if (is_operator(char(i)))
+			map[i] |= OPERATOR_MASK;
+	}
+
+	std::cout << "static int key_map[" << NKEYS << "] = {";
+	for (i = 0; i < NKEYS; i++)
+	{
+		if (i % 16 == 0)
+			std::cout << "\n\t";
+		std::cout << map[i] << ",";
+	}
+	std::cout << "\n};" << std::endl;
+}
+
+
+
+
+
+
+
+
+
+LexFile::LexFile (const std::string& fname, std::istream& is)
+	: _fname(fname), _badfile(!is.good())
+{
+	char buffer[512];
+	size_t num;
+
+	for (;;)
+	{
+		is.read(buffer, sizeof(buffer));
+		num = is.gcount();
+
+		if (num == 0)
+			break;
+
+		_data.insert(_data.end(), buffer, buffer + num);
+	}
+}
+LexFile::~LexFile () {}
+char LexFile::get (size_t pos) const
+{
+	if (pos >= _data.size())
+		return '\0';
+	else
+		return _data[pos];
+}
+std::string LexFile::get (size_t start, size_t end) const
+{
+	end = std::min(end, _data.size());
+	start = std::min(start, end);
+
+	if (start == end)
+		return "";
+	else
+		return std::string(_data.data() + start, end - start);
+}
+
+
+
+
+
+Lexer::Lexer ()
+	: _file(nullptr) {}
+
+Lexer::~Lexer () {}
+
+void Lexer::openFile (const std::string& filename)
+{
+	std::ifstream fs(filename);
+	_file = LexFile::make(filename, fs);
+
+	if (_file->badfile())
+		throw Span(_file).die("cannot open file");
+	else
+		_open();
+}
+void Lexer::openString (const std::string& contents, 
+					const std::string& filename)
+{
+	_file = LexFile::make(filename, contents);
+	_open();
+}
+
+
+void Lexer::_open ()
+{
+	_filepos = 0;
+	advance();
+}
+bool Lexer::_eof ()
+{
+	return _filepos >= _file->filesize();
+}
+
+char Lexer::_peek ()
+{
+	return _file->get(_filepos);
+}
+char Lexer::_adv ()
+{
+	auto c = _peek();
+	_filepos++;
+	return c;
+}
+
+void Lexer::_trim ()
+{
+	for (;;)
+	{
+		while (!_eof() && is_space(_peek()))
+			_adv();
+
+		if (_peek() == COMMENT)
+			while (!_eof() && _peek() != '\n')
+				_adv();
+		else
+			break;
+	}
+}
+
+void Lexer::_number (const std::string& str)
+{
+	int_t num = 0;
+
+	for (size_t i = 0; i < str.size(); i++)
+	{
+		if (!is_digit(str[i]))
+		{
+			auto sp = _current.span;
+			sp.start += i;
+			sp *= 1;
+
+			throw sp.die("invalid character in number literal");
+		}
+
+		num = (num * 10) + int_t(str[i] - '0');
+	}
+
+	_current.tok = tNumberInt;
+	_current.valueInt = num;
+}
+void Lexer::_ident ()
+{
+	while (is_ident(_peek()))
+		_adv();
+
+	_current.span.end = _filepos;
+
+	auto str = _current.span.data();
+
+	if (is_digit(str[0]))
+		_number(str);
+	else
+	{
+		for (auto& p : keywords)
+			if (p.first == str)
+			{
+				_current.tok = p.second;
+				return;
+			}
+
+		_current.tok = tIdent;
+		_current.str = str;
+	}
+}
+void Lexer::_string ()
+{
+	auto quote = _adv();
+
+	std::ostringstream ss;
+
+	while (_peek() != quote)
+	{
+		if (_eof())
+			throw (_current.span * 1).die(
+				"expected closing \" before <end-of-file>");
+
+		ss << _adv();
+	}
+	
+	_adv();
+
+	_current.span.end = _filepos;
+	_current.tok = tString;
+	_current.str = ss.str();
+}
+
+
+Token Lexer::advance ()
+{
+	auto old = _current;
+
+	_trim();
+
+	_current.span = Span(_file, _filepos);
+
+	if (_eof())
+	{
+		_current.tok = tEOF;
+	}
+	else if (is_control(_peek()))
+	{
+		_current.tok = _adv();
+		_current.span *= 1;
+	}
+	else if (_peek() == '\"')
+		_string();
+	else
+		_ident();
+
+	return old;
+}
+
+
+
+Token Lexer::expect (int tok)
+{
+	if (_current == tok)
+		return _current;
+	else
+	{
+		std::ostringstream ss;
+		ss << "expected " << Token::string(tok) << ", got "
+		                  << _current.string();
+		throw _current.die(ss.str());
+	}
+}
+void Lexer::expect (const std::string& word)
+{
+	std::ostringstream ss;
+	ss << "expected " << word << ", got " << _current.string();
+	throw _current.die(ss.str());
+}
+void Lexer::unexpect ()
+{
+	std::ostringstream ss;
+	ss << "unexpected token " << _current.string();
+	throw _current.die(ss.str());
+}
+Token Lexer::eat (int tok)
+{
+	expect(tok);
+	return advance();
+}
+Token Lexer::eat (int tok1, int tok2)
+{
+	if (_current == tok1 || _current == tok2)
+		return advance();
+	else
+	{
+		std::ostringstream ss;
+		ss << "expected " << Token::string(tok1) << " or "
+		                  << Token::string(tok2) << ", got "
+		                  << _current.string();
+		throw _current.die(ss.str());
+	}
+}
+
+
+
+
+
+
+
+
+
+std::runtime_error Span::die (const std::string& msg) const
+{
+	std::ostringstream ss;
+
+	if (file != nullptr)
+		ss << file->filename() << ": ";
+
+	ss << msg;
+
+	return std::runtime_error(ss.str());
+}
+
+Span Span::operator+ (const Span& other) const
+{
+	return Span(file, std::min(start, other.start),
+		              std::max(end, other.end));
+}
+
+
+bool Token::isOperator () const
+{
+	return tok == tIdent && is_operator(str[0]);
+}
+
+bool Token::operator== (int otherTok) const
+{
+	if (otherTok == tNumber)
+		return tok == tNumberReal || // blaze it
+	           tok == tNumberInt;
+	else
+		return tok == otherTok;
+}
+
+static inline std::string quoted (const std::string& str, char q = PRETTY_QUOTE)
+{
+	std::ostringstream ss;
+	ss << q << str << q;
+	return ss.str();
+}
+
+std::string Token::string (int tok)
+{
+	if (tok < 256)
+		return std::string(1, char(tok));
+	
+	switch (tok)
+	{
+	case tEOF:
+		return "<end-of-file>";
+	case tIdent:
+		return "<ident>";
+	case tString:
+		return "<string>";
+	case tNumber:
+	case tNumberInt:
+	case tNumberReal:
+		return "<number>";
+
+	default:
+		for (auto& p : Lexer::keywords)
+			if (p.second == tok)
+				return quoted(p.first, PRETTY_QUOTE_KEYWORD);
+		break;
+	}
+
+	return "??";
+}
+
+std::string Token::string () const
+{
+	std::ostringstream ss;
+
+	switch (tok)
+	{
+	case tIdent:
+		return quoted(str);
+
+	case tString:
+		ss << "\"";
+		for (size_t i = 0; i < str.size(); i++)
+			if (i >= MAX_STRING_DISPLAY_LEN)
+			{
+				ss << "...";
+				break;
+			}
+			else
+				ss << str[i];
+		ss << "\"";
+		return ss.str();
+
+	case tNumberInt:
+		ss << valueInt;
+		return ss.str();
+
+	case tNumberReal:
+		ss << valueReal;
+		return ss.str();
+
+	default:
+		return string(tok);
+	}
+}
+
+
---
+++ src/Lexer.h
@@ -0,0 +1,197 @@
+#pragma once
+#include "Jupiter.h"
+#include <sstream>
+#include <istream>
+#include <utility>
+
+enum
+{
+	// 0 .. 255  :=  [ ] ( ) { } . , ;
+
+	tEOF = 0x10000,	// <eof>
+	tNumber,
+	tNumberInt, 	// <int>
+	tNumberReal, 	// <real>
+	tIdent, 		// <ident>
+	tString, 		// <string>
+
+	tIf,
+	tThen,
+	tElse,
+	tFunc,
+	tLet,
+	tLoop,
+	tFor,
+	tTrue,
+	tFalse,
+
+	tEqual,         // =
+	tWildcard,		// _
+	tColon,			// :
+	tArrow,			// ->
+	tFatArrow,		// =>
+
+	tLambda = '\\', tSemicolon = ';',
+	tLParen = '(', tRParen = ')',
+	tLCurl = '{', tRCurl = '}',
+	tLBrack = '[', tRBrack = ']',
+	tComma = ',', tDot = '.'
+};
+
+
+
+class LexFile
+{
+public:
+	using ptr = std::shared_ptr<LexFile>;
+	~LexFile ();
+
+
+	static ptr make (const std::string& fname, std::istream& is)
+	{
+		return std::shared_ptr<LexFile>(
+			new LexFile(fname, is));
+	}
+	static ptr make (const std::string& fname, const std::string& contents)
+	{
+		std::istringstream ss(contents);
+
+		return std::shared_ptr<LexFile>(
+			new LexFile(fname, ss));
+	}
+
+
+	inline std::string filename () const { return _fname; }
+	inline size_t filesize () const { return _data.size(); }
+	inline bool badfile () const { return _badfile; }
+
+	char get (size_t pos) const;
+	std::string get (size_t start, size_t end) const;
+
+private:
+	LexFile (const std::string& fname, std::istream& is);
+
+	std::string _fname;
+	std::vector<char> _data;
+	bool _badfile;
+};
+
+struct Span
+{
+	LexFile::ptr file;
+	size_t start, end;
+
+	inline explicit
+	Span (const LexFile::ptr& _file,
+					size_t _start, size_t _end)
+		: file(_file), start(_start), end(_end) {}
+
+	inline explicit
+	Span (const LexFile::ptr& _file = nullptr,
+					size_t _start = 0)
+		: file(_file), start(_start), end(_start) {}
+
+	inline int length () const { return end - start; }
+
+	// combine spans 
+	Span operator+ (const Span& other) const;
+	
+	// '+' = add to length
+	inline Span operator+ (size_t len) const
+	{ return Span(file, start, end + len); }
+	
+	// '*' = set length
+	inline Span operator* (size_t len) const 
+	{ return Span(file, start, start + len); }
+
+	inline Span& operator*= (size_t len)
+	{
+		end = start + len;
+		return *this;
+	}
+
+	inline std::string data () const
+	{
+		if (file == nullptr)
+			return "";
+		else
+			return file->get(start, end);
+	}
+
+	std::runtime_error die (const std::string& msg) const;
+};
+
+struct Token
+{
+	static std::string string (int tok);
+
+	int tok;
+	Span span;
+
+	std::string str;
+	union
+	{
+		int_t valueInt;
+		real_t valueReal;
+	};
+
+	inline explicit Token (const Span& sp = Span())
+		: tok(tEOF), span(sp) {}
+
+	std::string string () const;
+	bool isOperator () const;
+
+	bool operator== (int tok) const;
+	inline bool operator!= (int tok) const
+	{
+		return !(*this == tok);
+	}
+
+	inline std::runtime_error die (const std::string& msg) const
+	{
+		return span.die(msg);
+	}
+
+};
+
+
+
+class Lexer
+{
+public:
+	Lexer ();
+	~Lexer ();
+
+	void openFile (const std::string& filename);
+	void openString (const std::string& contents, 
+						const std::string& filename = "<input>");
+
+	inline const Token& current () const { return _current; }
+
+	Token advance (); // returns old token and advances
+	Token expect (int tok); // if current() != tok { unexpect() }
+	void expect (const std::string& word);
+	void unexpect (); // throw "unexpected token"
+	Token eat (int tok); // expect(tok); advance()
+	Token eat (int tok1, int tok2); // tok1 or tok2
+
+
+	static std::vector<std::pair<std::string, int>> keywords;
+
+	static void generateKeyMaps ();
+private:
+	LexFile::ptr _file;
+	size_t _filepos;
+	Token _current;
+
+	void _open ();
+
+	void _trim ();
+	bool _eof ();
+	char _peek ();
+	char _adv ();
+
+	void _number (const std::string& str);
+	void _ident ();
+	void _string ();
+};
