4c3828b926d21b21eb00e250719f120b6178259e
Milo Turner <iitalics@gmail.com>
2015-04-04 22:23:56 -1000

    Added entry point

---
+++ bin/test.j
@@ -0,0 +1,6 @@
+{
+	let x = 4;
+	let y = 5;
+
+	println(x + y * 2)
+}
---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "Jupiter.h"
+#include "Parser.h"
+#include "Types.h"
+
+static int Main (std::vector<std::string>&& args);
+
+int main (int argc, char** argv)
+{
+	std::vector<std::string> args;
+	args.reserve(argc);
+
+	for (int i = 0; i < argc; i++)
+		args.push_back(argv[i]);
+
+	return Main(std::move(args));
+}
+
+static int Main (std::vector<std::string>&& args)
+{
+	if (args.size() > 1 && args[1] == "-keymaps")
+	{
+		Lexer::generateKeyMaps();
+		return 0;
+	}
+
+
+	try
+	{
+		Lexer lex;
+		lex.openFile("test.j");
+
+		auto e = Parse::parseExp(lex);
+		lex.expect(tEOF);
+
+		std::cout << e->string(true) << std::endl;
+
+		return 0;
+	}
+	catch (std::exception& err)
+	{
+		std::cout << "error: " << err.what() << std::endl;
+		return 1;
+	}
+}
