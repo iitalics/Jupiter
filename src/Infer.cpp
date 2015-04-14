68063971ea4cf98f6ed453eda0715e92a8bd790e
Milo Turner <iitalics@gmail.com>
2015-04-13 22:02:54 -1000

    Satisfactory new Subs structure

--- src/Infer.cpp
+++ src/Infer.cpp
@@ -1,2 +1,54 @@
 #include "Infer.h"
 
+
+
+Subs::Subs (const RuleList& _rules)
+	: rules(_rules) {}
+
+
+Subs Subs::operator+ (const Rule& rule) const
+{ return Subs(RuleList(rule, rules)); }
+
+Subs& Subs::operator+= (const Rule& rule)
+{
+	rules = RuleList(rule, rules);
+	return *this;
+}
+
+TyPtr Subs::operator() (TyPtr ty) const
+{ return apply(ty, rules.reverse()); }
+
+TyPtr Subs::apply (TyPtr ty, const RuleList& ru) const
+{
+	if (ru.nil())
+		return ty;
+
+	auto rule = ru.head();
+	auto tail = ru.tail();
+
+	switch (ty->kind)
+	{
+	case tyPoly:
+		if (rule.left == ty)
+			ty = rule.right;
+
+		return apply(ty, tail);
+
+	case tyConcrete:
+		if (ty->subtypes.size() == 0)
+			return ty;
+		else
+			return
+				Ty::makeConcrete(ty->name,
+					ty->subtypes.map([=] (TyPtr t)
+					{
+						return apply(t, ru);
+					}));
+
+	case tyOverloaded:
+	default:
+		return ty;
+	}
+}
+
+
--- src/Infer.h
+++ src/Infer.h
@@ -1,3 +1,29 @@
 #pragma once
 #include "Env.h"
 
+struct Infer;
+using InferPtr = std::shared_ptr<Infer>;
+using InferList = list<InferPtr>;
+
+
+
+struct Subs
+{
+	struct Rule
+	{
+		TyPtr left;
+		TyPtr right;
+	};
+	using RuleList = list<Rule>;
+
+	RuleList rules;
+
+	Subs (const RuleList& rules = RuleList());
+
+	Subs operator+ (const Rule& r) const;
+	Subs& operator+= (const Rule& r);
+	TyPtr operator() (TyPtr ty) const;
+
+private:
+	TyPtr apply (TyPtr ty, const RuleList& r) const;
+};
